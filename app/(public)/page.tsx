e00c66f8252e79fa7c414eedf08f6826528bf1fc
unknown <masd18784@gmail.com>
2026-04-12 22:07:14 +0700

    kontolodon

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,9 +1,11 @@
-import { Input } from "@/components/ui/input";
+import HeroSection from "@/components/hero-section";
+import FeaturesSection from "@/components/daftar-populer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-background">
-      <Input placeholder="Search..." />
+    <main className="min-h-screen bg-linear-to-b from-slate-50 to-slate-100">
+      <HeroSection />
+      <FeaturesSection />
     </main>
   );
 }
---
+++ components/daftar-terbaru.tsx
@@ -0,0 +1,39 @@
+import FeatureCard from "@/components/card";
+
+export default function FeaturesSection() {
+  const features = [
+    {
+      title: "kajian mbah gupron",
+      description: "yg pasti nya sesat dan menyimpang dari ajaran islam",
+    },
+    {
+      title: "ustadz ahmad sarwat",
+      description: "kajian untuk muhamadiah",
+    },
+    {
+      title: "ustad abdul somad",
+      description: "Engga usah di jelasin lah, udah pada tau kan",
+    },
+    {
+      title: "ustadz rendy suherman",
+      description: "tukang ngeloco naik haji",
+    }
+  ];
+
+  return (
+    <div id="populer" className="px-6 py-16 max-w-6xl mx-auto text-left">
+      <h2 className="text-3xl font-bold text-slate-900 mb-12">
+        Daftar populer
+      </h2>
+      <div className="grid md:grid-cols-3 gap-8">
+        {features.map((feature, i) => (
+          <FeatureCard
+            key={i}
+            title={feature.title}
+            description={feature.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
